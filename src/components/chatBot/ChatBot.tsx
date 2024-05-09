c78edd010089e421ec32ac5758d4e2a4f29f9907
nsanzimfura-eric <erickykress1@gmail.com>
2024-05-09 21:27:11 +0200

    Add ChatBot component

---
+++ src/components/chatBot/ChatBot.tsx
@@ -0,0 +1,12 @@
+import './ChatBot.scss';
+
+const ChatBot = () => {
+    return (
+        <div className='chatbot'>
+            ChatBot
+
+        </div>
+    );
+};
+
+export default ChatBot;
--- src/pages/WaterWave/WaterWave.tsx
+++ src/pages/WaterWave/WaterWave.tsx
@@ -1,3 +1,4 @@
+import ChatBot from '../../components/chatBot/ChatBot';
 import './WaterWave.scss';
 // @ts-ignore
 import WaterWave from 'react-water-wave';
@@ -11,31 +12,12 @@ const WaterWaveComp = () => {
             className="WaterWave"
 
         >
-            {({ pause, play }: { pause: () => void, play: () => void }) => {
+            {() => {
 
                 return (
                     <div className="container bg-none d-flex justify-content-center align-items-center">
-                        <h1>Water Wave</h1>
-                        <div className='d-flex mb-5'>
-                            <button className="btn me-2 bg-light" onClick={pause}>
-                                Pause
-                                🙅‍
-                            </button>
-                            <button className="btn bg-light" onClick={play}>
-                                Play{' '}
-                                🙆‍
-                            </button>
-                        </div>
-                        <div className='text-secondary'>
-                            MIT ©{' '}
-                            <a
-                                href="https://github.com/nsanzimfura-eric"
-                                rel="noopener noreferrer"
-                                target="_blank"
-                            >
-                                GitHub
-                            </a>
-                        </div>
+                        <h1>Hello, Plz Answer to these Questions, so that, we help you</h1>
+                        <ChatBot />
                     </div>
                 )
             }
