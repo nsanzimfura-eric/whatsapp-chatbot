118b737c1f509c141a7abbb8bdafc02323ca44ab
nsanzimfura-eric <erickykress1@gmail.com>
2024-05-10 10:44:34 +0200

    Modiy whatsapp link to start with launch message

--- src/components/chatBot/ChatBot.tsx
+++ src/components/chatBot/ChatBot.tsx
@@ -4,7 +4,7 @@ const ChatBot = () => {
     return (
         <div className='chatbot'>
             <a
-                href='https://api.whatsapp.com/send/?phone=250735317999&text&type=phone_number&app_absent=0'
+                href='https://api.whatsapp.com/send/?phone=250735317999&text=%23start%20LEAD'
                 target='_blank'
                 className='w-100 text-center'
             >
