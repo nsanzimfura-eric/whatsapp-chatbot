6ea15fe5bbe3f9344fc1b6aa670e261124d98e00
nsanzimfura-eric <erickykress1@gmail.com>
2024-05-10 10:19:51 +0200

    Create Link to chatbot

--- src/components/chatBot/ChatBot.tsx
+++ src/components/chatBot/ChatBot.tsx
@@ -3,7 +3,13 @@ import './ChatBot.scss';
 const ChatBot = () => {
     return (
         <div className='chatbot'>
-            ChatBot
+            <a
+                href='https://api.whatsapp.com/send/?phone=250735317999&text&type=phone_number&app_absent=0'
+                target='_blank'
+                className='w-100 text-center'
+            >
+                Click here to chat with us
+            </a>
 
         </div>
     );
